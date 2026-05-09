d0923cf0ac4646f610cd2621d10738165709a0b6
Agsergio04 <agsergio@g.educaand.es>
2026-05-09 19:03:55 +0200

    Creando la pagina de contenedores

---
+++ frontend/src/pages/contenedores/contenedores.jsx
@@ -0,0 +1,17 @@
+import './contenedores.scss'
+
+function Contenedores() {
+  return (
+    <div className="o-app-shell">
+      <header className="o-header">
+        <h1>Fluster</h1>
+      </header>
+      <aside className="o-sidebar" />
+      <main className="o-main">
+        <h2>Contenedores</h2>
+      </main>
+    </div>
+  )
+}
+
+export default Contenedores
--- frontend/src/router/AppRouter.jsx
+++ frontend/src/router/AppRouter.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import Login from '../pages/login/login'
 import Home from '../pages/home/home'
 import Almacen from '../pages/almacen/almacen'
+import Contenedores from '../pages/contenedores/contenedores'
 
 function AppRouter() {
   return (
@@ -10,6 +11,7 @@ function AppRouter() {
         <Route path="/login" element={<Login />} />
         <Route path="/" element={<Home />} />
         <Route path="/almacen" element={<Almacen />} />
+        <Route path="/contenedores" element={<Contenedores />} />
         <Route path="*" element={<Navigate to="/login" replace />} />
       </Routes>
     </BrowserRouter>
