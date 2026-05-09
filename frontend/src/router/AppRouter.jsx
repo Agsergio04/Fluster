d76f892c302701955334655198944125279a7234
Agsergio04 <agsergio@g.educaand.es>
2026-05-09 19:01:16 +0200

    Arreglando el rutamiento de la pagina login

--- frontend/src/router/AppRouter.jsx
+++ frontend/src/router/AppRouter.jsx
@@ -1,14 +1,14 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
-import LoginPage from '../pages/LoginPage'
-import HomePage from '../pages/HomePage'
+import Login from '../pages/login/login'
+import Home from '../pages/home/home'
 import Almacen from '../pages/almacen/almacen'
 
 function AppRouter() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Home />} />
         <Route path="/almacen" element={<Almacen />} />
         <Route path="*" element={<Navigate to="/login" replace />} />
       </Routes>
