634e5a8caf31335fcc44c7512bb5adb9f98f42bb
Agsergio04 <agsergio@g.educaand.es>
2026-05-13 09:41:58 +0200

    Arreglando problemas del header cuando vas a la landingPage

--- frontend/src/pages/home/home.jsx
+++ frontend/src/pages/home/home.jsx
@@ -1,6 +1,7 @@
 import { useNavigate } from 'react-router-dom'
 import './home.scss'
 import useTema from '../../hooks/useTema'
+import { getUsuario } from '../../services/session'
 import Header from '../../components/organismos/Header'
 import IntroduccionPagina from '../../components/organismos/IntroduccionPagina'
 import InformacionHome from '../../components/moleculas/InformacionHome'
@@ -8,13 +9,15 @@ import InformacionHome from '../../components/moleculas/InformacionHome'
 function Home() {
   const navigate = useNavigate()
   const [tema, toggleTema] = useTema()
+  const usuario = getUsuario()
 
   return (
     <div className="home">
       <Header
-        rol={null}
+        rol={usuario?.rol ?? null}
         tema={tema}
         onToggleTema={toggleTema}
+        onNavegar={ruta => navigate(ruta)}
       />
 
       <IntroduccionPagina
