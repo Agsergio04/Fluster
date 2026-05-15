9ecc07317e09bd5ea4b0e45817047db96b441114
Agsergio04 <agsergio@g.educaand.es>
2026-05-15 17:30:13 +0200

    style: tabla tarifas se divide en 2 bloques en movil - tiempo y costes por separado

--- frontend/src/components/organismos/TablaTarifas.jsx
+++ frontend/src/components/organismos/TablaTarifas.jsx
@@ -1,22 +1,29 @@
 import CabeceraTablasTarifasNavieras from '../moleculas/CabeceraTablasTarifasNavieras'
 import FilaNavieraTarifasConBotones from '../moleculas/FilaNavieraTarifasConBotones'
+import TablaTarifasMovil from './TablaTarifasMovil'
 
 function TablaTarifas({ filas = [] }) {
   return (
-    <div className="tabla-tarifas-wrapper">
-      <div className="tabla-tarifas">
-        <CabeceraTablasTarifasNavieras />
-        {filas.map((fila, i) => (
-          <FilaNavieraTarifasConBotones
-            key={fila.naviera ?? i}
-            naviera={fila.naviera}
-            valores={fila.valores}
-            onActualizar={fila.onActualizar}
-            onEliminar={fila.onEliminar}
-          />
-        ))}
+    <>
+      <div className="tabla-tarifas-wrapper tabla-tarifas-wrapper--desktop">
+        <div className="tabla-tarifas">
+          <CabeceraTablasTarifasNavieras />
+          {filas.map((fila, i) => (
+            <FilaNavieraTarifasConBotones
+              key={fila.naviera ?? i}
+              naviera={fila.naviera}
+              valores={fila.valores}
+              onActualizar={fila.onActualizar}
+              onEliminar={fila.onEliminar}
+            />
+          ))}
+        </div>
       </div>
-    </div>
+
+      <div className="tabla-tarifas-wrapper--movil">
+        <TablaTarifasMovil filas={filas} />
+      </div>
+    </>
   )
 }
 
